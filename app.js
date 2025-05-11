2bcb3b798b7189aa578890892dbad2bfd3254f11
Mohammed Minhaj <nijulinux@gmail.com>
2025-05-11 12:02:14 +0530

    feat: basic project controller done

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ require("dotenv").config({ path: `${process.cwd()}/.env` });
 
 const express = require("express");
 
-const authRoute = require("./route/authRouter");
 const catchAsync = require("./utils/catchAsync");
 const AppError = require('./utils/appError');
 const globalErrorHandler = require("./controller/errorContoller");
+
+const authRoute = require("./route/authRouter");
+const projectRoute = require('./route/projectRoute');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(`/api/v1/auth`, authRoute);
+app.use(`/api/v1/project`,projectRoute);
 
 app.use(
   "/{*any}",
