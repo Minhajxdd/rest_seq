a33f2b8da17c7246a8930398eab3d81276a5f1a4
Mohammed Minhaj <nijulinux@gmail.com>
2025-05-11 12:14:33 +0530

    updated

--- controller/authController.js
+++ controller/authController.js
@@ -49,13 +49,13 @@ const login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(new AppError('Please provide email and password', 400));
+    return next(new AppError("Please provide email and password", 400));
   }
 
   const result = await user.findOne({ where: { email } });
 
   if (!result || !(await bcrypt.compare(password, result.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    return next(new AppError("Incorrect email or password", 401));
   }
 
   const token = generateToken({
@@ -68,4 +68,30 @@ const login = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = { signup, login };
+const authentication = catchAsync(async (req, res, next) => {
+  let idToken = "";
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    idToken = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!idToken) {
+    return next(new AppError(`Please login to get access`, 401));
+  }
+
+  const tokenDetail = jwt.verify(idToken, process.env.JWT_SECRET_KEY);
+
+  const freshUser = await user.findByPk(tokenDetail.id);
+
+  if (!freshUser) {
+    return next(new AppError("User no longer exists", 400));
+  }
+
+  req.user = freshUser;
+
+  return next();
+});
+
+module.exports = { signup, login, authentication };
--- route/projectRoute.js
+++ route/projectRoute.js
@@ -1,7 +1,8 @@
 const { createProject } = require("../controller/projectController");
+const { authentication } = require("../controller/authController");
 
 const router = require("express").Router();
 
-router.route("/").post(createProject);
+router.route("/").post(authentication, createProject);
 
 module.exports = router;
